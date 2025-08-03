7a08af972b1823ff626a2659e98995a8bcea139f
victor <str259@mail.ru>
2025-08-03 19:46:00 +0700

    Initial commit 8

--- tests/server_test.go
+++ tests/server_test.go
@@ -1,13 +1,10 @@
 package grpc_demon
 
 import (
-	"fmt"
 	"log"
-	"strconv"
 	"testing"
 
 	"grpc_demon/model"
-	"grpc_demon/storage"
 )
 
 // тестируем загрузку конфига метрик
@@ -24,28 +21,29 @@ func TestMetricsInit(t *testing.T) {
 	}
 }
 
-// тестируем работу метрики
-func TestMetricsGetValue(t *testing.T) {
+// // тестируем работу метрики
+// func TestMetricsGetValue(t *testing.T) {
 
-	metric := &model.Metric{
-		Name:        "Load avg. (%)",
-		Cache:       &storage.Cache{},
-		Cmd:         "cat",
-		CmdParams:   "/proc/loadavg", //nolint:
-		ParseParams: "{print $1}",    //nolint:
-		IsActive:    true,
-	}
+// 	metric := &model.Metric{
+// 		Name:        "Load avg. (%)",
+// 		Cache:       &storage.Cache{},
+// 		Cmd:         "cat",
+// 		CmdParams:   "/proc/loadavg",
+// 		ParseParams: "{print $1}",
+// 		IsActive:    true,
+// 	}
 
-	stringValue, err := metric.GetValue()
-	if err != nil {
-		log.Fatal(err)
-	}
+// 	stringValue, err := metric.GetValue()
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
 
-	floatValue, err := strconv.ParseFloat(stringValue, 64)
+// 	floatValue, err := strconv.ParseFloat(stringValue, 64)
 
-	if err != nil {
-		log.Fatal(err)
-	}
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
 
-	fmt.Println(floatValue)
-}
+// 	fmt.Println(floatValue)
+
+// }
